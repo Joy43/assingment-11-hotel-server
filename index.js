19a41b2a5e1c6047d66d8494f63d364077a64c9e
joy43 <ssjoy43@gmail.com>
2023-11-05 00:23:49 +0600

    Mongodb setup sever connect

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 
 
 
+/*
+ hotel
+PpxL2n6fPNYYxOfw 
+*/
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://hotel:PpxL2n6fPNYYxOfw@cluster0.ovpumir.mongodb.net/?retryWrites=true&w=majority";
 
-/* hotel
-PpxL2n6fPNYYxOfw */
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
