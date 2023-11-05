d1b4b1a862e69714e405ff4f55f7c7e83be883cb
joy43 <ssjoy43@gmail.com>
2023-11-05 13:23:02 +0600

    sevices data connect server

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+const express =require('express');
+require ('dotenv').config()
+const cors = require ('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
 
-
-
+// -----------middle ware---------
+app.use(cors({
+  origin:[
+    'http://localhost:5173'
+  ],
+  credentials:true,
+})
+)
 /*
  hotel
 PpxL2n6fPNYYxOfw 
 */
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://hotel:PpxL2n6fPNYYxOfw@cluster0.ovpumir.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +35,11 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+// -------------service connect---------------
+
+const serviceCollection =client.db('Hotelmanage').collection('services');
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -31,4 +48,12 @@ async function run() {
     // await client.close();
   }
 }
+
+// -----------------
 run().catch(console.dir);
+app.get('/',(req,res) =>{
+  res.send('Hotel  is runing' )
+})
+app.listen(port,()=>{
+  console.log(`Hotel  server is running  on port${port}n`)
+})
